78e1aa27e483d446e8a2a48cc52d760a59078e55
Miguel Ángel Díaz <miguelangel.diaz@47deg.com>
2019-07-01 12:29:05 +0200

    init project

---
+++ xcode/nef/AppDelegate.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2019 The nef Authors.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
