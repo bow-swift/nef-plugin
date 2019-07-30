2b6371842555d01036905cdef8e6f31c64c2b7bc
Miguel Ángel Díaz <miguelangel.diaz@47deg.com>
2019-07-30 10:32:33 +0200

    preferences model conform to Equatable

--- nef/Preferences/Data/PreferencesModel.swift
+++ nef/Preferences/Data/PreferencesModel.swift
@@ -3,11 +3,21 @@
 import Foundation
 import NefModels
 
-struct PreferencesModel: Codable {
+struct PreferencesModel {
     let showLines: Bool
     let showWatermark: Bool
     let font: CarbonStyle.Font
     let theme: CarbonStyle.Theme
     let size: CarbonStyle.Size
     let color: CarbonStyle.Color
+}
+
+extension PreferencesModel: Codable {}
+extension PreferencesModel: Equatable {
+    static func == (lhs: PreferencesModel, rhs: PreferencesModel) -> Bool {
+        return lhs.showLines == rhs.showLines &&
+               lhs.font == rhs.font &&
+               lhs.theme == rhs.theme &&
+               lhs.color == rhs.color
+    }
 }
