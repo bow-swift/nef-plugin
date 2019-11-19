cfa34a000296b6dafaeb271b27754ad4fdc6cd2b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-11-19 17:56:47 +0100

    remove unuseful code

--- nef/Utils/DispatchGroup.swift
+++ nef/Utils/DispatchGroup.swift
@@ -4,17 +4,6 @@ import Foundation
 
 
 func runSync<T>(on queue: DispatchQueue = .main, block: @escaping () -> T?) -> T? {
-    guard !DispatchQueue.isMainThread else { return block() }
+    guard !Thread.isMainThread else { return block() }
     return queue.sync { block() }
-}
-
-
-extension DispatchQueue {
-    public static var currentLabel: String {
-        return String(validatingUTF8: __dispatch_queue_get_label(nil)) ?? ""
-    }
-    
-    public static var isMainThread: Bool {
-        currentLabel == "com.apple.main-thread"
-    }
 }
