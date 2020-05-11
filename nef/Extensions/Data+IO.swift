81844eb711782f383e1abefe45fa74066844009e
Juan Navas <juannavas7@gmail.com>
2020-05-11 19:50:31 +0200

    add Data extension to make an NSImage

--- nef/Extensions/Data+IO.swift
+++ nef/Extensions/Data+IO.swift
@@ -3,11 +3,25 @@
 import Foundation
 import Bow
 import BowEffects
+import AppKit
+
+enum ImageError: Error {
+    case invalidData
+}
 
 extension Data {
     func writeIO(to url: URL) -> IO<Error, Void> {
         IO.invoke {
             try self.write(to: url)
+        }
+    }
+    
+    func makeImage() -> IO<ImageError, NSImage> {
+        IO.invoke {
+            guard let image = NSImage(data: self) else {
+                throw ImageError.invalidData
+            }
+            return image
         }
     }
 }
