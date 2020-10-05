c226018115ffdb99b50b61432a4cc7f54f30ce0b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-10-05 11:39:26 +0200

    group menu-editor options in URLQueryItem maker

--- nef-models/MenuEditorCommand+URLQueryItem.swift
+++ nef-models/MenuEditorCommand+URLQueryItem.swift
@@ -5,13 +5,7 @@ import Foundation
 public extension MenuEditorCommand {
     func item(code: String) -> URLQueryItem {
         switch self {
-        case .exportSnippetToFile:
-            return URLQueryItem(name: key, value: code)
-        case .exportSnippetToClipboard:
-            return URLQueryItem(name: key, value: code)
-        case .markdownPage:
-            return URLQueryItem(name: key, value: code)
-        case .playgroundBook:
+        case .exportSnippetToFile, .exportSnippetToClipboard, .markdownPage, .playgroundBook:
             return URLQueryItem(name: key, value: code)
         case .preferences, .about:
             return URLQueryItem(name: key, value: "")
